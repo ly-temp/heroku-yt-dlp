fc77bad6d7656e377a5692bb38dfd228edd62e88
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-09-10 11:08:02 +0800

    Update my_yt-dlp.sh

--- bash/my_yt-dlp.sh
+++ bash/my_yt-dlp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #input: [url] [audio_only?] [format]
 if [ "$2" = "1" ]; then
-  output=$(../yt-dlp.sh "$1" -f "bestaudio[ext=webm]/bestaudio[ext=m4a]")
+  output=$(../yt-dlp_linux "$1" -f "bestaudio[ext=webm]/bestaudio[ext=m4a]")
 else
   if [ "$3" = "" ]; then
-    output=$(../yt-dlp.sh "$1")
+    output=$(../yt-dlp_linux "$1")
   else
-    output=$(../yt-dlp.sh "$1" -f "$3")
+    output=$(../yt-dlp_linux "$1" -f "$3")
   fi
 fi
 filename=$(echo "$output" | grep "Destination:" | grep -o ":.*" | cut -d":" -f2-);
